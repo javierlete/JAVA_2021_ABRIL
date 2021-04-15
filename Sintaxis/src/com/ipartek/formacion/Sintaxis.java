13d8a297e3336e9558ca98ea3752a3418b33cd38
Javier Lete <javier@eskura21.com>
2021-04-15 17:40:38 +0200

    Tipos primitivos

---
+++ Sintaxis/src/com/ipartek/formacion/Sintaxis.java
@@ -0,0 +1,44 @@
+package com.ipartek.formacion;
+
+/**
+ * Clase que muestra la sintaxis básica de Java
+ * @author JavierLete
+ *
+ */
+public class Sintaxis {
+	/**
+	 * Método en el que se inicia la aplicación
+	 * @param args argumentos recibidos por consola
+	 */
+	public static void main(String[] args) {
+		// Comentario de línea
+		
+		/*
+		 * Comentario
+		 * multi
+		 * línea
+		 */
+		
+		// TIPOS PRIMITIVOS
+		// ================
+		// ENTEROS (CON SIGNO) -2^(n-1) a 2^(n-1)-1
+		
+		// byte		 8bits	-128 a 127
+		// short	16bits	-32768 a 32767
+		// int		32bits	-2147M a 2147M (El más habitual)
+		// long		64bits	-9T a 9T
+		
+		// COMA FLOTANTE (IEEE754)
+		
+		// float	32bits
+		// double	64bits
+		
+		// BOOLEANO
+		
+		// boolean	16bits
+		
+		// CARACTER
+		
+		// char		16bits (UNICODE)
+	}
+}
