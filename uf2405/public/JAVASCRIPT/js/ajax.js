aeb864a10aed4d7024a6bec68bc1861dd7fa36e5
Javier Lete <javier@eskura21.com>
2021-05-26 16:25:39 +0200

    Ejemplo AJAX de mantenimiento

--- uf2405/public/JAVASCRIPT/js/ajax.js
+++ uf2405/public/JAVASCRIPT/js/ajax.js
@@ -1,19 +1,83 @@
+const URL = 'http://localhost:3000/clientes/';
+
 document.addEventListener('DOMContentLoaded', async () => {
-    const respuesta = await fetch('json/clientes.json');
+    const respuesta = await fetch(URL);
     const clientes = await respuesta.json();
 
     console.log(clientes);
 
-    const ul = document.getElementById('lista');
-    let li;
+    const tbody = document.querySelector('tbody');
+    let tr;
 
     clientes.forEach((cliente) => {
-        li = document.createElement('li');
-        li.innerHTML = `${cliente.nombre} ${cliente.apellido}`;
+        tr = document.createElement('tr');
+        tr.innerHTML = `
+            <th>${cliente.id}</th>
+            <td>${cliente.nombre}</td>
+            <td>${cliente.apellido}</td>
+            <td>
+                <a href="javascript:editar(${cliente.id})">Editar</a>
+                <a href="javascript:borrar(${cliente.id})">Borrar</a>
+            </td>`;
 
-        ul.appendChild(li);
+        tbody.appendChild(tr);
+    });
+
+    document.querySelector('form').addEventListener('submit', async (e) => {
+        e.preventDefault();
+        console.log('ACEPTAR');
+
+        const id = document.querySelector('#id').value;
+        const nombre = document.querySelector('#nombre').value;
+        const apellido = document.querySelector('#apellido').value;
+
+        const cliente = { nombre, apellido };
+
+        console.log(cliente);
+
+        // debugger;
+
+        if (id) {
+            cliente.id = id;
+            await fetch(URL + id, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(cliente)
+            });
+        } else {
+            await fetch(URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(cliente)
+            });
+        }
     });
 });
+
+async function editar(id) {
+    console.log('EDITAR', id);
+
+    const respuesta = await fetch(URL + id);
+    const cliente = await respuesta.json();
+
+    console.log(cliente);
+
+    document.getElementById("id").value = cliente.id;
+    document.getElementById("nombre").value = cliente.nombre;
+    document.getElementById("apellido").value = cliente.apellido;
+}
+
+async function borrar(id) {
+    console.log('BORRAR', id);
+
+    await fetch(URL + id, { method: 'DELETE' });
+
+    console.log('Borrado');
+}
 
 // ES2015
 // document.addEventListener('DOMContentLoaded', function () {
