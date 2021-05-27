3ed36003b4daf4bf6baafae7a9ad6695d1605e58
Javier Lete <javier@eskura21.com>
2021-05-27 16:22:12 +0200

    jQuery AJAX para mantenimiento

--- uf2405/public/JQUERY/js/ajax.js
+++ uf2405/public/JQUERY/js/ajax.js
@@ -1,13 +1,48 @@
-$(function() {
-    console.log('1');
-    $.getJSON('json/clientes.json', function(clientes) {
-        console.log('2');
-        $(clientes).each(function() {
-            console.log('3');
-            $('ul').append($('<li>').html(this.nombre + ' ' + this.apellido));
-        });
+var URL = 'http://localhost:3000/clientes/';
 
-        console.log('4');
+$(function () {
+    $.getJSON(URL, function (clientes) {
+        $(clientes).each(function () {
+            $('<tr><td>' + this.id + '</td><td>' + this.nombre + '</td><td>' + this.apellido +
+                '</td><td><a href="javascript:editar(' + this.id +
+                ')">Editar</a> <a href="javascript:borrar(' + this.id +
+                ')">Borrar</a></td></tr>').appendTo('tbody');
+        });
     });
-    console.log('5');
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        var numId = parseInt($('#id').val());
+        var id = isNaN(numId) ? null : numId;
+        var cliente = { id, nombre: $('#nombre').val(), apellido: $('#apellido').val() };
+        
+        console.log(cliente);
+
+        var metodo = cliente.id ? 'PUT' : 'POST';
+        var url = cliente.id ? URL + id : URL;
+
+        $.ajax(url, { method: metodo, data: cliente });
+    });
 });
+
+function editar(id) {
+    console.log('EDITAR', id);
+
+    if (id) {
+        $.getJSON(URL + id, function (cliente) {
+            $('#id').val(cliente.id);
+            $('#nombre').val(cliente.nombre);
+            $('#apellido').val(cliente.apellido);
+        });
+    } else {
+        $('#id').val('');
+        $('#nombre').val('');
+        $('#apellido').val('');
+    }
+}
+
+function borrar(id) {
+    console.log('BORRAR', id);
+
+    $.ajax(URL + id, { method: 'DELETE' });
+}
