4eccea2a400abfffb7ad181655c6df8bd1cd1fbd
Javier Lete <javier@eskura21.com>
2021-05-28 15:55:59 +0200

    AJAX con refresco de pantalla

--- uf2405/public/JQUERY/js/ajax.js
+++ uf2405/public/JQUERY/js/ajax.js
@@ -2,21 +2,7 @@ var URL = 'http://localhost:3000/clientes/';
 
 // Cuando se cargue el documento...
 $(function () {
-    // Ejecutamos una petición GET a la URL del json-server
-    $.getJSON(URL, function (clientes) {
-        // Cuando llegue la respuesta del servidor...
-        // por cada cliente dentro del lote de clientes recibido...
-        $(clientes).each(function () {
-            // Creamos una fila de una tabla con la información de cada cliente que tenemos en this
-            // y la agregamos (appendTo) a el cuerpo de la tabla (tbody)
-            $('<tr><td>' + this.id + '</td><td>' + this.nombre + '</td><td>' + this.apellido +
-                // href="javascript:" implica que se llamará a la función que ponemos detrás (editar o borrar)
-                // cuando alguien pulse ese vínculo con un argumento que es el id del cliente
-                '</td><td><a class="btn btn-primary" href="javascript:editar(' + this.id +
-                ')">Editar</a> <a class="btn btn-danger" href="javascript:borrar(' + this.id +
-                ')">Borrar</a></td></tr>').appendTo('tbody');
-        });
-    });
+    refrescarTabla();
 
     // Cuando se envíe el formulario...
     $('form').on('submit', function(e) {
@@ -42,9 +28,29 @@ $(function () {
 
         // Lanzamos la petición a la url correspondiente con el método correspondiente y con los datos del cliente metidos
         // en el objeto cliente
-        $.ajax(url, { method: metodo, data: cliente });
+        $.ajax(url, { method: metodo, data: cliente }).then(refrescarTabla);
     });
 });
+
+function refrescarTabla() {
+    // Ejecutamos una petición GET a la URL del json-server
+    $.getJSON(URL, function (clientes) {
+        // Cuando llegue la respuesta del servidor...
+        // ...vaciamos la tabla de registros y ...
+        $('tbody').empty();
+        // por cada cliente dentro del lote de clientes recibido...
+        $(clientes).each(function () {
+            // Creamos una fila de una tabla con la información de cada cliente que tenemos en this
+            // y la agregamos (appendTo) a el cuerpo de la tabla (tbody)
+            $('<tr><td>' + this.id + '</td><td>' + this.nombre + '</td><td>' + this.apellido +
+                // href="javascript:" implica que se llamará a la función que ponemos detrás (editar o borrar)
+                // cuando alguien pulse ese vínculo con un argumento que es el id del cliente
+                '</td><td><a class="btn btn-primary" href="javascript:editar(' + this.id +
+                ')">Editar</a> <a class="btn btn-danger" href="javascript:borrar(' + this.id +
+                ')">Borrar</a></td></tr>').appendTo('tbody');
+        });
+    });
+}
 
 // Se llamará cuando pulsemos un enlace de editar
 function editar(id) {
@@ -76,5 +82,5 @@ function editar(id) {
 function borrar(id) {
     console.log('BORRAR', id);
     // Mandamos la petición de tipo DELETE a la URL http://localhost:3000/clientes/2
-    $.ajax(URL + id, { method: 'DELETE' });
+    $.ajax(URL + id, { method: 'DELETE' }).then(refrescarTabla);
 }
