1eb805f33282a7abbd7b0a159a19df28e7ca0107
Javier Lete <javier@eskura21.com>
2021-06-10 16:27:21 +0200

    Insertar y modificar funcional

--- uf2406/src/main/java/com/ipartek/formacion/uf2406/controladores/admin/AdminProductosDetalleServlet.java
+++ uf2406/src/main/java/com/ipartek/formacion/uf2406/controladores/admin/AdminProductosDetalleServlet.java
@@ -1,6 +1,8 @@
 package com.ipartek.formacion.uf2406.controladores.admin;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,11 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ipartek.formacion.uf2406.accesodatos.ProductoDao;
+import com.ipartek.formacion.uf2406.modelos.Producto;
 
 @WebServlet("/admin/producto")
 public class AdminProductosDetalleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");
 		
@@ -23,4 +27,28 @@ public class AdminProductosDetalleServlet extends HttpServlet {
 		request.getRequestDispatcher("/WEB-INF/vistas/admin/productodetalle.jsp").forward(request, response);
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String id = request.getParameter("id");
+		String nombre = request.getParameter("nombre");
+		String precio = request.getParameter("precio");
+		
+		Long lId = null;
+		
+		if(id != null && id.trim().length() > 0) {
+			lId = Long.parseLong(id);
+		}
+		
+		Producto producto = new Producto(lId, nombre, new BigDecimal(precio));
+		
+		if(lId != null) {
+			ProductoDao.modificar(producto);
+		} else {
+			ProductoDao.insertar(producto);
+		}
+		
+		response.sendRedirect(request.getContextPath() + "/admin/productos");
+	}
 }
