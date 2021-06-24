3d4fad9bdd23f313afdd2ebe62c38a48003a6616
Javier Lete <javier@eskura21.com>
2021-06-24 16:10:14 +0200

    Ejemplo JDBC con el resto de SQL

--- uf2175/src/uf2175/EjemploJDBC.java
+++ uf2175/src/uf2175/EjemploJDBC.java
@@ -10,6 +10,65 @@ public class EjemploJDBC {
 		String password = "admin";
 
 		Connection con = DriverManager.getConnection(url, usuario, password);
+		
+		ResultSet rs;
+		mostrarRegistros(con);
+		
+		String id = "2";
+		
+		PreparedStatement ps = con.prepareStatement("SELECT * FROM personas WHERE id = ?");
+		ps.setInt(1, Integer.parseInt(id));
+		//rs = st.executeQuery("SELECT * FROM personas WHERE id = " + id);
+		rs = ps.executeQuery();
+		
+		while (rs.next()) {
+			System.out.println(String.format("%s, %s, %s", 
+					rs.getInt("id"), rs.getString("nombre"), rs.getString("apellidos")));
+		}
+		
+		String nombre = "Insertado";
+		String apellidos = "Insertadez";
+		
+		ps = con.prepareStatement("INSERT INTO personas (nombre, apellidos) VALUES (?, ?)");
+		
+		ps.setString(1, nombre);
+		ps.setString(2, apellidos);
+		
+		int numeroRegistrosModificados = ps.executeUpdate();
+		
+		System.out.println(numeroRegistrosModificados);
+		
+		mostrarRegistros(con);
+		
+		nombre = "Modificado";
+		apellidos = "Modificadez";
+		
+		ps = con.prepareStatement("UPDATE personas SET nombre=?, apellidos=? WHERE id=?");
+		
+		ps.setString(1, nombre);
+		ps.setString(2, apellidos);
+		ps.setInt(3, Integer.parseInt(id));
+		
+		numeroRegistrosModificados = ps.executeUpdate();
+		
+		System.out.println(numeroRegistrosModificados);
+		
+		mostrarRegistros(con);
+		
+		ps = con.prepareStatement("DELETE FROM personas WHERE id=?");
+		
+		ps.setInt(1, Integer.parseInt(id));
+		
+		numeroRegistrosModificados = ps.executeUpdate();
+		
+		System.out.println(numeroRegistrosModificados);
+		
+		mostrarRegistros(con);
+		
+		con.close();
+	}
+
+	private static void mostrarRegistros(Connection con) throws SQLException {
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery("SELECT * FROM personas");
 
@@ -20,7 +79,6 @@ public class EjemploJDBC {
 		
 		rs.close();
 		st.close();
-		con.close();
 	}
 
 }
