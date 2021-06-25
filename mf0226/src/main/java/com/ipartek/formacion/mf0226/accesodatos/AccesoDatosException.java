260ec7de15f6d7030b19d17fc8888e4e3e13a719
Javier Lete <javier@eskura21.com>
2021-06-25 18:40:21 +0200

    Cogemos accesodatos, entidades y jar de MySQL

---
+++ mf0226/src/main/java/com/ipartek/formacion/mf0226/accesodatos/AccesoDatosException.java
@@ -0,0 +1,29 @@
+package com.ipartek.formacion.mf0226.accesodatos;
+
+public class AccesoDatosException extends RuntimeException {
+
+	private static final long serialVersionUID = 2083743418047125739L;
+
+	public AccesoDatosException() {
+		super();
+	}
+
+	public AccesoDatosException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public AccesoDatosException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public AccesoDatosException(String message) {
+		super(message);
+	}
+
+	public AccesoDatosException(Throwable cause) {
+		super(cause);
+	}
+	
+	
+}
