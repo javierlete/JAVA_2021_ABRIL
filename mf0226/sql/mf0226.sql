943e043ec409f2a0e13a0e692dd5227a51c81505
Javier Lete <javier@eskura21.com>
2021-07-19 16:53:07 +0200

    Copia de la base de datos con sueldo

--- mf0226/sql/mf0226.sql
+++ mf0226/sql/mf0226.sql
@@ -18,6 +18,61 @@ USE `mf0226`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `bolsas`
+--
+
+DROP TABLE IF EXISTS `bolsas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bolsas` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `empresa` varchar(45) NOT NULL,
+  `usuarios_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_bolsas_usuarios1_idx` (`usuarios_id`),
+  CONSTRAINT `fk_bolsas_usuarios1` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bolsas`
+--
+
+LOCK TABLES `bolsas` WRITE;
+/*!40000 ALTER TABLE `bolsas` DISABLE KEYS */;
+INSERT INTO `bolsas` VALUES (3,'pepe@email.net',2),(4,'pepe@email.net',2);
+/*!40000 ALTER TABLE `bolsas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bolsas_has_personas`
+--
+
+DROP TABLE IF EXISTS `bolsas_has_personas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bolsas_has_personas` (
+  `bolsas_id` bigint NOT NULL,
+  `personas_id` bigint NOT NULL,
+  PRIMARY KEY (`bolsas_id`,`personas_id`),
+  KEY `fk_bolsas_has_personas_personas1_idx` (`personas_id`),
+  KEY `fk_bolsas_has_personas_bolsas1_idx` (`bolsas_id`),
+  CONSTRAINT `fk_bolsas_has_personas_bolsas1` FOREIGN KEY (`bolsas_id`) REFERENCES `bolsas` (`id`),
+  CONSTRAINT `fk_bolsas_has_personas_personas1` FOREIGN KEY (`personas_id`) REFERENCES `personas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bolsas_has_personas`
+--
+
+LOCK TABLES `bolsas_has_personas` WRITE;
+/*!40000 ALTER TABLE `bolsas_has_personas` DISABLE KEYS */;
+INSERT INTO `bolsas_has_personas` VALUES (3,1),(4,1),(3,3),(4,12),(4,14),(4,15);
+/*!40000 ALTER TABLE `bolsas_has_personas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `ocupaciones`
 --
 
@@ -38,7 +93,7 @@ CREATE TABLE `ocupaciones` (
 
 LOCK TABLES `ocupaciones` WRITE;
 /*!40000 ALTER TABLE `ocupaciones` DISABLE KEYS */;
-INSERT INTO `ocupaciones` VALUES (1,'Programador',NULL),(2,'Contable',NULL);
+INSERT INTO `ocupaciones` VALUES (1,'Programador','Los que programan'),(2,'Contable',NULL);
 /*!40000 ALTER TABLE `ocupaciones` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,11 +108,12 @@ CREATE TABLE `personas` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `apellidos` varchar(100) DEFAULT NULL,
+  `sueldo` decimal(10,2) DEFAULT NULL,
   `ocupaciones_id` bigint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_personas_ocupaciones_idx` (`ocupaciones_id`),
   CONSTRAINT `fk_personas_ocupaciones` FOREIGN KEY (`ocupaciones_id`) REFERENCES `ocupaciones` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +122,7 @@ CREATE TABLE `personas` (
 
 LOCK TABLES `personas` WRITE;
 /*!40000 ALTER TABLE `personas` DISABLE KEYS */;
-INSERT INTO `personas` VALUES (1,'Javier','Lete',1),(3,'MODIFICADO','MODIFICADEZ',2),(4,'Insertado','Insertadezeteasdfasdfsdf',1),(9,'afsd','dfgh',1),(12,'dsfhgsdfghdfgh','hjklhjkl',1),(13,'sdfgsdfg','sdfgsdfg',2),(14,'sdfgsdfg','dfghdhfg',1),(15,'sdfgsdfg','sdfgsdfg',1);
+INSERT INTO `personas` VALUES (1,'Javier','Lete',23456.00,1),(3,'MODIFICADO','MODIFICADEZ',12345.00,2),(4,'Insertado','Insertadezeteasdfasdfsdf',1234.56,1),(9,'afsd','dfgh',NULL,1),(12,'dsfhgsdfghdfgh','hjklhjkl',12345.67,1),(13,'sdfgsdfg','sdfgsdfg',NULL,2),(14,'sdfgsdfg','dfghdhfg',432.12,1),(15,'sdfgsdfg','sdfgsdfg',NULL,1),(16,'asdf','dfgh',54233.00,2);
 /*!40000 ALTER TABLE `personas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,14 +152,6 @@ LOCK TABLES `usuarios` WRITE;
 INSERT INTO `usuarios` VALUES (1,'javier@email.net','contra','ADMIN'),(2,'pepe@email.net','perez','USER');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'mf0226'
---
-
---
--- Dumping routines for database 'mf0226'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -114,4 +162,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-06 20:06:19
+-- Dump completed on 2021-07-19 16:52:30
