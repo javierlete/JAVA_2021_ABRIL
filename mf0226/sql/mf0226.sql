ae7b4f5a55838e5b1b7f8896eda9689e88c86fb9
Javier Lete <javier@eskura21.com>
2021-07-23 17:07:12 +0200

    Procedimientos almacenados, cursores, triggers, índices

--- mf0226/sql/mf0226.sql
+++ mf0226/sql/mf0226.sql
@@ -84,7 +84,7 @@ CREATE TABLE `ocupaciones` (
   `nombre` varchar(45) NOT NULL,
   `descripcion` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +93,7 @@ CREATE TABLE `ocupaciones` (
 
 LOCK TABLES `ocupaciones` WRITE;
 /*!40000 ALTER TABLE `ocupaciones` DISABLE KEYS */;
-INSERT INTO `ocupaciones` VALUES (1,'Programador','Los que programan'),(2,'Contable',NULL);
+INSERT INTO `ocupaciones` VALUES (1,'Programador','Los que programan'),(2,'Contable',NULL),(3,'Administrativo',NULL);
 /*!40000 ALTER TABLE `ocupaciones` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -112,8 +112,9 @@ CREATE TABLE `personas` (
   `ocupaciones_id` bigint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_personas_ocupaciones_idx` (`ocupaciones_id`),
+  KEY `fk_personas_nombre` (`nombre`),
   CONSTRAINT `fk_personas_ocupaciones` FOREIGN KEY (`ocupaciones_id`) REFERENCES `ocupaciones` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -122,9 +123,26 @@ CREATE TABLE `personas` (
 
 LOCK TABLES `personas` WRITE;
 /*!40000 ALTER TABLE `personas` DISABLE KEYS */;
-INSERT INTO `personas` VALUES (1,'Javier','Lete',23456.00,1),(3,'MODIFICADO','MODIFICADEZ',12345.00,2),(4,'Insertado','Insertadezeteasdfasdfsdf',1234.56,1),(9,'afsd','dfgh',NULL,1),(12,'dsfhgsdfghdfgh','hjklhjkl',12345.67,1),(13,'sdfgsdfg','sdfgsdfg',NULL,2),(14,'sdfgsdfg','dfghdhfg',432.12,1),(15,'sdfgsdfg','sdfgsdfg',NULL,1),(16,'asdf','dfgh',54233.00,2);
+INSERT INTO `personas` VALUES (1,'Javier','Lete',23456.00,1),(3,'MODIFICADO','MODIFICADEZ',12345.00,3),(4,'Insertado','Insertadezeteasdfasdfsdf',1234.56,1),(9,'afsd','dfgh',NULL,3),(12,'dsfhgsdfghdfgh','hjklhjkl',12345.67,1),(13,'sdfgsdfg','sdfgsdfg',NULL,2),(14,'sdfgsdfg','dfghdhfg',432.12,1),(15,'sdfgsdfg','sdfgsdfg',NULL,3),(16,'asdf','dfgh',54233.00,2),(17,'Prueba','asdf',12345.00,2);
 /*!40000 ALTER TABLE `personas` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `personas_BEFORE_INSERT` BEFORE INSERT ON `personas` FOR EACH ROW BEGIN
+	INSERT INTO historico VALUES (NEW.nombre);
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `usuarios`
@@ -160,6 +178,96 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'mf0226'
 --
+/*!50003 DROP FUNCTION IF EXISTS `fsumar` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `fsumar`(a INT, b INT) RETURNS int
+    DETERMINISTIC
+BEGIN
+
+RETURN a + b;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `cursores` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cursores`()
+BEGIN
+
+DECLARE vid BIGINT;
+DECLARE vnombre VARCHAR(50);
+DECLARE vapellidos VARCHAR(100);
+DECLARE vsueldo DECIMAL(10,2);
+
+DECLARE final INT;
+DECLARE fila CURSOR FOR SELECT id, nombre, apellidos, sueldo FROM personas;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET final = 1;
+
+SET final = 0;
+
+OPEN fila;
+
+FETCH fila INTO vid, vnombre, vapellidos, vsueldo;
+
+WHILE final <> 1 DO 
+	IF vid = 4 THEN
+		SELECT vid, vnombre, vapellidos, vsueldo;
+	END IF;
+	
+    FETCH fila INTO vid, vnombre, vapellidos, vsueldo;
+END WHILE;
+
+CLOSE fila;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `ejemplo` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ejemplo`(IN entrada VARCHAR(50), INOUT entradasalida VARCHAR(50), OUT salida VARCHAR(50))
+BEGIN
+	SELECT 'ANTES', entrada, entradasalida, salida;
+
+	SET entrada = entrada + 1;
+    SET entradasalida = entradasalida + 1;
+    SET salida = 10;
+    
+	SELECT 'DESPUES', entrada, entradasalida, salida;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `guguel` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -194,6 +302,90 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `max_sueldos` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `max_sueldos`()
+BEGIN
+
+DECLARE vnombre VARCHAR(50);
+DECLARE vapellidos VARCHAR(100);
+DECLARE vsueldo DECIMAL(10,2);
+DECLARE vocupacion VARCHAR(50);
+
+DECLARE vocupacion_anterior VARCHAR(50);
+
+DECLARE final INT;
+
+DECLARE fila CURSOR FOR SELECT p.nombre, p.apellidos, p.sueldo, o.nombre
+FROM personas p
+JOIN ocupaciones o ON p.ocupaciones_id = o.id
+ORDER BY o.nombre, sueldo DESC;
+
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET final = 1;
+
+SET vocupacion_anterior = '';
+SET final = 0;
+
+DROP TABLE IF EXISTS temporal;
+
+CREATE TEMPORARY TABLE temporal (
+  `nombre` varchar(50) NOT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `sueldo` decimal(10,2) DEFAULT NULL,
+  `ocupacion` varchar(50) NOT NULL
+);
+
+OPEN fila;
+
+FETCH fila INTO vnombre, vapellidos, vsueldo, vocupacion;
+
+WHILE final <> 1 DO 
+	IF vocupacion_anterior <> vocupacion THEN
+		INSERT INTO temporal VALUES (vnombre, vapellidos, vsueldo, vocupacion);
+        
+        SET vocupacion_anterior = vocupacion;
+	END IF;
+	
+    FETCH fila INTO vnombre, vapellidos, vsueldo, vocupacion;
+END WHILE;
+
+CLOSE fila;
+
+SELECT * FROM temporal;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sumar` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sumar`(IN a INT, IN b INT, OUT total INT)
+BEGIN
+	SET total = a + b;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -204,4 +396,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-22 16:37:28
+-- Dump completed on 2021-07-23 17:06:36
