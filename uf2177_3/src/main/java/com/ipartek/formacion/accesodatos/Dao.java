3ceb57cead60a62d84ef002db2015e87ae2512a8
Javier Lete <javier@eskura21.com>
2021-09-13 16:27:09 +0200

    Dao genérico

---
+++ uf2177_3/src/main/java/com/ipartek/formacion/accesodatos/Dao.java
@@ -0,0 +1,23 @@
+package com.ipartek.formacion.accesodatos;
+
+public interface Dao<T> {
+	default Iterable<T> obtenerTodos() {
+		throw new AccesoDatosException("No implementado");
+	}
+
+	default T obtenerPorId(long id) {
+		throw new AccesoDatosException("No implementado");
+	}
+
+	default T insertar(T objeto) {
+		throw new AccesoDatosException("No implementado");
+	}
+
+	default T modificar(T objeto) {
+		throw new AccesoDatosException("No implementado");
+	}
+
+	default void borrar(long id) {
+		throw new AccesoDatosException("No implementado");
+	}
+}
