ceb536a0258987b359d86d9893ac4613f9894a4d
Javier Lete <javier@eskura21.com>
2021-09-15 19:48:20 +0200

    REST controller con gestión de mensajes de error y normales

--- uf2177_3_spring/src/main/java/com/ipartek/formacion/uf2177spring/controladores/rest/ClienteRestController.java
+++ uf2177_3_spring/src/main/java/com/ipartek/formacion/uf2177spring/controladores/rest/ClienteRestController.java
@@ -1,6 +1,8 @@
 package com.ipartek.formacion.uf2177spring.controladores.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ipartek.formacion.uf2177spring.accesodatos.Dao;
@@ -25,11 +28,18 @@ public class ClienteRestController {
 	}
 
 	@GetMapping("{id}")
-	public Cliente getConId(@PathVariable Long id) {
-		return dao.obtenerPorId(id);
+	public ResponseEntity<Cliente> getConId(@PathVariable Long id) {
+		Cliente cliente = dao.obtenerPorId(id);
+		
+		if(cliente != null) {
+			return new ResponseEntity<>(cliente, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente post(@RequestBody Cliente cliente) {
 		return dao.insertar(cliente);
 	}
@@ -40,6 +50,7 @@ public class ClienteRestController {
 	}
 	
 	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		dao.borrar(id);
 	}
