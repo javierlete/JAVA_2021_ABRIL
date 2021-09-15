0726a7c2eb601f6edf72613bc0142e8b67a3c3df
Javier Lete <javier@eskura21.com>
2021-09-15 19:15:13 +0200

    Proyecto uf2177_3_spring (REST)

---
+++ uf2177_3_spring/src/main/java/com/ipartek/formacion/uf2177spring/Uf21773SpringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.uf2177spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uf21773SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uf21773SpringApplication.class, args);
+	}
+
+}
---
+++ uf2177_3_spring/src/test/java/com/ipartek/formacion/uf2177spring/Uf21773SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.uf2177spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uf21773SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
