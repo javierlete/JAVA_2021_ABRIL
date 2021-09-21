d1e0021dc2c03aa86df1e7fd5dad53a9a22193a9
Javier Lete <javier@eskura21.com>
2021-09-21 18:10:01 +0200

    Capa de acceso a datos

---
+++ mf0226spring/src/main/java/com/ipartek/formacion/mf0226spring/accesodatos/Dao.java
@@ -0,0 +1,23 @@
+package com.ipartek.formacion.mf0226spring.accesodatos;
+
+public interface Dao<T> {
+	public default Iterable<T> obtenerTodos() {
+		throw new AccesoDatosException("No implementado");
+	}
+
+	public default T obtenerPorId(Integer id) {
+		throw new AccesoDatosException("No implementado");
+	}
+
+	public default T insertar(T objeto) {
+		throw new AccesoDatosException("No implementado");
+	}
+
+	public default T modificar(T objeto) {
+		throw new AccesoDatosException("No implementado");
+	}
+
+	public default void borrar(Integer id) {
+		throw new AccesoDatosException("No implementado");
+	}
+}
---
+++ mf0226spring/src/main/java/com/ipartek/formacion/mf0226spring/accesodatos/DaoMySqlCurso.java
@@ -0,0 +1,44 @@
+package com.ipartek.formacion.mf0226spring.accesodatos;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Repository;
+
+import com.ipartek.formacion.mf0226spring.modelos.Curso;
+import com.ipartek.formacion.mf0226spring.modelos.Profesor;
+
+@Repository
+public class DaoMySqlCurso implements Dao<Curso> {
+
+	private static final String URL = "jdbc:mysql://localhost:3306/gestiondocente";
+	private static final String USUARIO = "root";
+	private static final String PASSWORD = "admin";
+	private static final String SQL_SELECT = "SELECT * FROM curso c JOIN profesor p ON c.profesor_codigo = p.codigo";
+	
+	@Override
+	public Iterable<Curso> obtenerTodos() {
+		try (Connection con = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+				PreparedStatement ps = con.prepareStatement(SQL_SELECT);
+				ResultSet rs = ps.executeQuery()) {
+			ArrayList<Curso> cursos = new ArrayList<Curso>();
+			Profesor profesor;
+			
+			while(rs.next()) {
+				profesor = new Profesor(rs.getInt("p.codigo"), rs.getString("p.nombre"), rs.getString("p.apellidos"));
+				cursos.add(new Curso((Integer)rs.getObject("c.codigo"), rs.getString("c.nombre"), rs.getString("c.identificador"), rs.getDate("c.fInicio").toLocalDate(), rs.getDate("c.fFin").toLocalDate(), rs.getInt("c.nHoras"), rs.getString("c.temario"), rs.getBoolean("c.activo"), rs.getBigDecimal("c.precio"), profesor));
+			}
+			
+			cursos.add(new Curso(1, "Prueab", "asdfasdf", LocalDate.now(), LocalDate.now(), 5, "", true, new BigDecimal("34"), new Profesor(1, "Javier", "lete")));
+			
+			return cursos;
+		} catch (Exception e) {
+			throw new AccesoDatosException("Error al obtener los cursos", e);
+		}
+	}
+}
