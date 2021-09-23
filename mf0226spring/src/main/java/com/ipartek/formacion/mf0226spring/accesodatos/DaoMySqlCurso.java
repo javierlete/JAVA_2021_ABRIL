8369f09ac7e8129c3807126b529e1e2fb2ed4187
Javier Lete <javier@eskura21.com>
2021-09-23 16:12:12 +0200

    JdbcTemplate en ejemplo

--- mf0226spring/src/main/java/com/ipartek/formacion/mf0226spring/accesodatos/DaoMySqlCurso.java
+++ mf0226spring/src/main/java/com/ipartek/formacion/mf0226spring/accesodatos/DaoMySqlCurso.java
@@ -1,11 +1,7 @@
 package com.ipartek.formacion.mf0226spring.accesodatos;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.ipartek.formacion.mf0226spring.modelos.Alumno;
@@ -16,75 +12,50 @@ import com.ipartek.formacion.mf0226spring.modelos.Resena;
 @Repository
 public class DaoMySqlCurso implements Dao<Curso> {
 
-	private static final String URL = "jdbc:mysql://localhost:3306/gestiondocente";
-	private static final String USUARIO = "root";
-	private static final String PASSWORD = "admin";
+	@Autowired
+	private JdbcTemplate jdbc;
+
 	private static final String SQL_SELECT = "SELECT * FROM curso c JOIN profesor p ON c.profesor_codigo = p.codigo";
 	private static final String SQL_SELECT_ID = SQL_SELECT + " WHERE c.codigo = ?";
 	private static final String SQL_SELECT_RESENAS = "SELECT \r\n"
-			+ "    r.texto AS texto, a.codigo AS codigo, a.nombre AS nombre, a.apellidos AS apellidos\r\n"
-			+ "FROM\r\n"
-			+ "    resenas r\r\n"
-			+ "        JOIN\r\n"
-			+ "    alumno a ON r.alumno_codigo = a.codigo\r\n"
-			+ "WHERE\r\n"
+			+ "    r.texto AS texto, a.codigo AS codigo, a.nombre AS nombre, a.apellidos AS apellidos\r\n" + "FROM\r\n"
+			+ "    resenas r\r\n" + "        JOIN\r\n" + "    alumno a ON r.alumno_codigo = a.codigo\r\n" + "WHERE\r\n"
 			+ "    curso_codigo = ?;";
-	
+
 	@Override
 	public Iterable<Curso> obtenerTodos() {
-		try (Connection con = DriverManager.getConnection(URL, USUARIO, PASSWORD);
-				PreparedStatement ps = con.prepareStatement(SQL_SELECT);
-				ResultSet rs = ps.executeQuery()) {
-			ArrayList<Curso> cursos = new ArrayList<Curso>();
-			Profesor profesor;
-			
-			while(rs.next()) {
-				profesor = new Profesor(rs.getInt("p.codigo"), rs.getString("p.nombre"), rs.getString("p.apellidos"));
-				cursos.add(new Curso((Integer)rs.getObject("c.codigo"), rs.getString("c.nombre"), rs.getString("c.identificador"), rs.getDate("c.fInicio").toLocalDate(), rs.getDate("c.fFin").toLocalDate(), rs.getInt("c.nHoras"), rs.getString("c.temario"), rs.getBoolean("c.activo"), rs.getBigDecimal("c.precio"), profesor));
-			}
-			
-			return cursos;
-		} catch (Exception e) {
-			throw new AccesoDatosException("Error al obtener los cursos", e);
-		}
+		return jdbc.query(SQL_SELECT, (rs, rowNum) -> {
+			Profesor profesor = new Profesor(rs.getInt("p.codigo"), rs.getString("p.nombre"),
+					rs.getString("p.apellidos"));
+			Curso curso = new Curso((Integer) rs.getObject("c.codigo"), rs.getString("c.nombre"),
+					rs.getString("c.identificador"), rs.getDate("c.fInicio").toLocalDate(),
+					rs.getDate("c.fFin").toLocalDate(), rs.getInt("c.nHoras"), rs.getString("c.temario"),
+					rs.getBoolean("c.activo"), rs.getBigDecimal("c.precio"), profesor);
+			return curso;
+		});
 	}
 
 	@Override
 	public Curso obtenerPorId(Integer id) {
-		try (Connection con = DriverManager.getConnection(URL, USUARIO, PASSWORD);
-				PreparedStatement ps = con.prepareStatement(SQL_SELECT_ID);
-				) {
-			ps.setInt(1, id);
-			ResultSet rs = ps.executeQuery();
-			Curso curso = null;
-			Profesor profesor;
-			
-			if(rs.next()) {
-				profesor = new Profesor(rs.getInt("p.codigo"), rs.getString("p.nombre"), rs.getString("p.apellidos"));
-				curso = new Curso((Integer)rs.getObject("c.codigo"), rs.getString("c.nombre"), rs.getString("c.identificador"), rs.getDate("c.fInicio").toLocalDate(), rs.getDate("c.fFin").toLocalDate(), rs.getInt("c.nHoras"), rs.getString("c.temario"), rs.getBoolean("c.activo"), rs.getBigDecimal("c.precio"), profesor);
-				
-				PreparedStatement psResenas = con.prepareStatement(SQL_SELECT_RESENAS);
-				psResenas.setInt(1, id);
-				
-				ResultSet rsResenas = psResenas.executeQuery();
-				
-				ArrayList<Resena> resenas = new ArrayList<>();
-				Resena resena = null;
-				Alumno alumno = null;
-				
-				while(rsResenas.next()) {
-					alumno = new Alumno(rsResenas.getInt("codigo"), rsResenas.getString("nombre"), rsResenas.getString("apellidos"));
-					resena = new Resena(alumno, null, rsResenas.getString("texto"));
-					
-					resenas.add(resena);
-				}
-				
-				curso.setResenas(resenas);
-			}
-			
-			return curso;
-		} catch (Exception e) {
-			throw new AccesoDatosException("Error al obtener el curso " + id, e);
+		Curso curso = (Curso) jdbc.queryForObject(SQL_SELECT_ID, (rs, rowNum) -> {
+			Profesor profesor = new Profesor(rs.getInt("p.codigo"), rs.getString("p.nombre"),
+					rs.getString("p.apellidos"));
+			Curso c = new Curso((Integer) rs.getObject("c.codigo"), rs.getString("c.nombre"),
+					rs.getString("c.identificador"), rs.getDate("c.fInicio").toLocalDate(),
+					rs.getDate("c.fFin").toLocalDate(), rs.getInt("c.nHoras"), rs.getString("c.temario"),
+					rs.getBoolean("c.activo"), rs.getBigDecimal("c.precio"), profesor);
+			return c;
+		}, id);
+
+		if (curso != null) {
+			Iterable<Resena> resenas = jdbc.query(SQL_SELECT_RESENAS, (rs, rowNum) -> {
+				Alumno alumno = new Alumno(rs.getInt("codigo"), rs.getString("nombre"), rs.getString("apellidos"));
+				Resena resena = new Resena(alumno, null, rs.getString("texto"));
+				return resena;
+			}, id);
+			curso.setResenas(resenas);
 		}
+
+		return curso;
 	}
 }
