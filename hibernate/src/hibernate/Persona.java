72d0854ed8d9a1763018afd72e40f5c54d97236a
Javier Lete <javier@eskura21.com>
2021-09-28 16:25:39 +0200

    Anotaciones

--- hibernate/src/hibernate/Persona.java
+++ hibernate/src/hibernate/Persona.java
@@ -2,13 +2,29 @@ package hibernate;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data  @AllArgsConstructor @NoArgsConstructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "personas")
 public class Persona {
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long id;
 	private String nombre;
+	@Column(name = "fecha_nacimiento")
 	private LocalDate fechaNacimiento;
 }
