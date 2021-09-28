a814b8f703c3158f371d82350419d962b7df804d
Javier Lete <javier@eskura21.com>
2021-09-28 16:20:32 +0200

    Ejemplo con todas las operaciones de CRUD

--- hibernate/src/hibernate/HibernatePrueba.java
+++ hibernate/src/hibernate/HibernatePrueba.java
@@ -1,6 +1,7 @@
 package hibernate;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -16,9 +17,48 @@ public class HibernatePrueba {
 		Session session = factory.openSession();
 		session.beginTransaction();
 		
+		// INSERTAR
 		session.save(new Persona(null, "Pepe", LocalDate.now()));
 		session.save(new Persona(null, "Juan", LocalDate.now()));
 		session.save(new Persona(null, "Pedro", LocalDate.now()));
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		session = factory.openSession();
+		session.beginTransaction();
+		
+		// OBTENER POR ID
+		Persona persona2 = session.find(Persona.class, 2L);
+		
+		System.out.println(persona2);
+		
+		// MODIFICAR DESDE BUSQUEDA
+		persona2.setNombre("Modificado");
+		
+		// MODIFICAR DESDE OBJETO NUEVO
+		Persona persona3 = new Persona(3L, "Modificado3", null);
+		
+		session.merge(persona3);
+		
+		// BORRAR
+		session.delete(new Persona(1L, null, null));
+		
+		// session.remove(session.find(Persona.class, 1L));
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		session = factory.openSession();
+		session.beginTransaction();
+		
+		// OBTENER TODOS
+		@SuppressWarnings("unchecked")
+		List<Persona> personas = session.createQuery("from Persona").list();
+		
+		for(Persona persona: personas) {
+			System.out.println(persona);
+		}
 		
 		session.getTransaction().commit();
 		session.close();
