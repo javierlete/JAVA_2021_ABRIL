788a60c9ba4eaff69afbf3525a85a155a60843a6
Javier Lete <javier@eskura21.com>
2021-09-29 16:17:39 +0200

    Spring Data inicio

---
+++ springdata/src/test/java/com/ipartek/formacion/springdata/SpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
