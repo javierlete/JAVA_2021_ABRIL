6b2b0f69772431a4650c1e685e93480e175ac9c1
Javier Lete <javier@eskura21.com>
2021-09-29 16:57:43 +0200

    Rest Repositories

--- springdata/src/main/java/com/ipartek/formacion/springdata/repositorios/PersonaRepository.java
+++ springdata/src/main/java/com/ipartek/formacion/springdata/repositorios/PersonaRepository.java
@@ -1,9 +1,11 @@
 package com.ipartek.formacion.springdata.repositorios;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.formacion.springdata.entidades.Persona;
 
+@RepositoryRestResource(collectionResourceRel = "personas", path = "personas")
 public interface PersonaRepository extends CrudRepository<Persona, Long> {
 	Persona findByNombre(String nombre);
 }
