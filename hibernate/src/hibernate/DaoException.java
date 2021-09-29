2fa7a504b78a5c86f6375f88d8e7cf01fc751b08
Javier Lete <javier@eskura21.com>
2021-09-29 16:08:24 +0200

    DAO con excepciones

---
+++ hibernate/src/hibernate/DaoException.java
@@ -0,0 +1,27 @@
+package hibernate;
+
+public class DaoException extends RuntimeException {
+
+	public DaoException() {
+		super();
+	}
+
+	public DaoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public DaoException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DaoException(String message) {
+		super(message);
+	}
+
+	public DaoException(Throwable cause) {
+		super(cause);
+	}
+
+	private static final long serialVersionUID = -2388542065955797273L;
+
+}
--- hibernate/src/hibernate/DaoMySqlPersona.java
+++ hibernate/src/hibernate/DaoMySqlPersona.java
@@ -8,74 +8,109 @@ import javax.persistence.Persistence;
 
 public class DaoMySqlPersona implements Dao<Persona> {
 
-	public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory( "hibernate" );
-	
-	public EntityManager abrirTransaccion()
-	{
+	public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("hibernate");
+
+	public EntityManager abrirTransaccion() {
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
-		
+
 		return em;
 	}
-	
-	public void cerrarTransaccion(EntityManager em)
-	{
+
+	public void cerrarTransaccion(EntityManager em) {
 		em.getTransaction().commit();
 		em.close();
 	}
-	
+
+	private void deshacerTransaccion(EntityManager em) {
+		if (em != null) {
+			em.getTransaction().rollback();
+			em.close();
+		}
+	}
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public Iterable<Persona> obtenerTodos() {
-		EntityManager em = abrirTransaccion();
-		
-		List<Persona> personas = em.createQuery("from Persona").getResultList();
-		
-		cerrarTransaccion(em);
-		
-		return personas;
+		EntityManager em = null;
+		try {
+			em = abrirTransaccion();
+
+			List<Persona> personas = em.createQuery("from Persona").getResultList();
+
+			cerrarTransaccion(em);
+
+			return personas;
+		} catch (Exception e) {
+			deshacerTransaccion(em);
+			throw new DaoException("Ha habido un error al consultar todos los registros", e);
+		}
 	}
 
 	@Override
 	public Persona obtenerPorId(Long id) {
-		EntityManager em = abrirTransaccion();
-		
-		Persona persona = em.find(Persona.class, id);
-		
-		cerrarTransaccion(em);
-		
-		return persona;
+		EntityManager em = null;
+		try {
+			em = abrirTransaccion();
+
+			Persona persona = em.find(Persona.class, id);
+
+			cerrarTransaccion(em);
+
+			return persona;
+		} catch (Exception e) {
+			deshacerTransaccion(em);
+			throw new DaoException("Ha habido un error al consultar el registro " + id, e);
+		}
 	}
 
 	@Override
 	public Persona insertar(Persona persona) {
-		EntityManager em = abrirTransaccion();
-		
-		em.persist(persona);
-		
-		cerrarTransaccion(em);
-		
-		return persona;
+		EntityManager em = null;
+		try {
+			em = abrirTransaccion();
+
+			em.persist(persona);
+
+			cerrarTransaccion(em);
+
+			return persona;
+		} catch (Exception e) {
+			deshacerTransaccion(em);
+			throw new DaoException("Ha habido un error al insertar el registro ", e);
+		}
 	}
 
 	@Override
 	public Persona modificar(Persona persona) {
-		EntityManager em = abrirTransaccion();
-		
-		em.merge(persona);
-		
-		cerrarTransaccion(em);
-		
-		return persona;
+		EntityManager em = null;
+		try {
+			em = abrirTransaccion();
+
+			em.merge(persona);
+
+			cerrarTransaccion(em);
+
+			return persona;
+		} catch (Exception e) {
+			deshacerTransaccion(em);
+			throw new DaoException("Ha habido un error al modificar el registro", e);
+		}
 	}
 
 	@Override
 	public void borrar(Long id) {
-		EntityManager em = abrirTransaccion();
-		
-		em.remove(em.find(Persona.class, id));
-		
-		cerrarTransaccion(em);
+		EntityManager em = null;
+		try {
+			em = abrirTransaccion();
+
+			em.remove(em.find(Persona.class, id));
+
+			cerrarTransaccion(em);
+		} catch (Exception e) {
+			deshacerTransaccion(em);
+			throw new DaoException("Ha habido un error al borrar el registro " + id, e);
+		}
 	}
 
 }
