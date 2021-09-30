c8bfaf70bdc8d950de95b35d3f5414a470a00faa
Javier Lete <javier@eskura21.com>
2021-09-30 15:48:42 +0200

    Inicio Spring Data Relaciones

---
+++ springdatarelaciones/src/main/java/com/ipartek/formacion/springdatarelaciones/SpringdatarelacionesApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.springdatarelaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatarelacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatarelacionesApplication.class, args);
+	}
+
+}
---
+++ springdatarelaciones/src/test/java/com/ipartek/formacion/springdatarelaciones/SpringdatarelacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.springdatarelaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatarelacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
