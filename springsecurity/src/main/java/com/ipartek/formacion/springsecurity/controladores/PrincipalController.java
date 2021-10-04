7d06c5338e3e18394785c52ee5e8c5b94f40cdaf
Javier Lete <javier@eskura21.com>
2021-10-04 15:48:01 +0200

    Antes de la autenticación

---
+++ springsecurity/src/main/java/com/ipartek/formacion/springsecurity/controladores/PrincipalController.java
@@ -0,0 +1,16 @@
+package com.ipartek.formacion.springsecurity.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalController {
+	@GetMapping("/")
+	public String principal() {
+		return "index";
+	}
+	@GetMapping("/admin")
+	public String admin() {
+		return "admin/index";
+	}
+}
