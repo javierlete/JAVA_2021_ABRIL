c15ef2a2f3cd17f3e9114702f51e3c62e37a952b
Javier Lete <javier@eskura21.com>
2021-10-07 18:00:28 +0200

    Corrección de refresco y eventos

--- springrestjavascript/src/main/webapp/js/index.js
+++ springrestjavascript/src/main/webapp/js/index.js
@@ -3,93 +3,93 @@ var URL = 'http://localhost:8080/api/personas';
 $(function() {
 	$('body > button').click(function() {
 		$('form')[0].reset();
-		
-		$('form button').html('Insertar').click(function() {
-			$.ajax({
-				url: URL,
-				method: 'POST',
-				data: JSON.stringify({ nombre: $('#nombre').val(), fechaNacimiento: $('#fechaNacimiento').val() }),
-				dataType: 'json',
-				contentType: 'application/json',
-			}).done(function(datos, estado, peticion){
-				alert('Todo correcto');
-				$('form').hide();
-			}).fail(function(peticion, estado, error){
-				alert('Ha habido un error');
-				
-				console.log(peticion);
-				console.log(estado);
-				console.log(error);
-			});
+
+		$('form button').html('Insertar').off('click').click(function() {
+		$.ajax({
+			url: URL,
+			method: 'POST',
+			data: JSON.stringify({ nombre: $('#nombre').val(), fechaNacimiento: $('#fechaNacimiento').val() }),
+			dataType: 'json',
+			contentType: 'application/json',
+		}).done(function(datos, estado, peticion) {
+			$('form').hide();
+			refrescarTabla();
+		}).fail(function(peticion, estado, error) {
+			alert('Ha habido un error');
+
+			console.log(peticion);
+			console.log(estado);
+			console.log(error);
 		});
-		
+	});;
+
+
+
 		$('form').show();
 	});
-	
+
 	$('form').hide();
+
+	refrescarTabla();
+});
+
+function refrescarTabla() {
+	$('tbody').empty();
 
 	$.getJSON(URL, function(datos) {
 		var personas = datos._embedded.personas;
 
 		console.log(personas);
 
-		var pre = $('body').append('<pre>');
-
 		$(personas).each(function() {
-			pre.append(this.nombre + ', ' + this.fechaNacimiento);
-			pre.append('<br/>');
-		});
-
-		$(personas).each(function() {
-			$('table').append('<tr><td>' + this.nombre + '</td><td>' + this.fechaNacimiento + '</td><td><a href="javascript:editar(\'' + this._links.self.href + '\')">Editar</a> <a href="javascript:borrar(\'' + this._links.self.href + '\')">Borrar</a></td></tr>');
+			$('tbody').append('<tr><td>' + this.nombre + '</td><td>' + this.fechaNacimiento + '</td><td><a href="javascript:editar(\'' + this._links.self.href + '\')">Editar</a> <a href="javascript:borrar(\'' + this._links.self.href + '\')">Borrar</a></td></tr>');
 		});
 	});
-});
+}
 
 function editar(href) {
 	console.log('Editar ' + href);
 
 	$.getJSON(href, function(persona) {
 		console.log(persona);
-		
+
 		$('#nombre').val(persona.nombre);
 		$('#fechaNacimiento').val(persona.fechaNacimiento);
 
-		$('form button').html('Editar').click(function() {
+		$('form button').html('Editar').off('click').click(function() {
 			$.ajax({
 				url: href,
 				method: 'PUT',
 				data: JSON.stringify({ nombre: $('#nombre').val(), fechaNacimiento: $('#fechaNacimiento').val() }),
 				dataType: 'json',
 				contentType: 'application/json',
-			}).done(function(datos, estado, peticion){
-				alert('Todo correcto');
+			}).done(function(datos, estado, peticion) {
 				$('form').hide();
-			}).fail(function(peticion, estado, error){
+				refrescarTabla();
+			}).fail(function(peticion, estado, error) {
 				alert('Ha habido un error');
-				
+
 				console.log(peticion);
 				console.log(estado);
 				console.log(error);
 			});
 		});
-
+;
 		$('form').show();
 	});
-
-
-
 }
 
 function borrar(href) {
-	alert('Borrar ' + href);
+	console.log('Borrar ' + href);
 
 	$.ajax({
 		url: href,
 		method: 'DELETE'
+	}).done(function() {
+		refrescarTabla();
 	}).fail(function(peticion, estado, error) {
 		alert('Ha habido un error');
-		
+
 		console.log(peticion);
 		console.log(estado);
 		console.log(error);
