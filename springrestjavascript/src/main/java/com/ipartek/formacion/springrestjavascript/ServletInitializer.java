c84c7ba63ca3ca36077678090836bef99c2d9d69
Javier Lete <javier@eskura21.com>
2021-10-07 16:33:50 +0200

    Inicio springrestjavascript

---
+++ springrestjavascript/src/main/java/com/ipartek/formacion/springrestjavascript/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.springrestjavascript;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringrestjavascriptApplication.class);
+	}
+
+}
---
+++ springrestjavascript/src/main/java/com/ipartek/formacion/springrestjavascript/SpringrestjavascriptApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.springrestjavascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestjavascriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestjavascriptApplication.class, args);
+	}
+
+}
