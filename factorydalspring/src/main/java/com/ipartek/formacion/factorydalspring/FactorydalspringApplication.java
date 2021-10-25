0bb37bf99e5bbccc9a7535881083725823baae95
Javier Lete <javier@eskura21.com>
2021-10-25 15:34:08 +0200

    Proyecto factorydalspring

---
+++ factorydalspring/src/main/java/com/ipartek/formacion/factorydalspring/FactorydalspringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.factorydalspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorydalspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorydalspringApplication.class, args);
+	}
+
+}
---
+++ factorydalspring/src/test/java/com/ipartek/formacion/factorydalspring/FactorydalspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.factorydalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorydalspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
